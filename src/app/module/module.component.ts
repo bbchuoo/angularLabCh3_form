23e65b5b0891e8ca5194011ecf900e099ee9883b
bbchu22 <bbchu22@digiwin.com>
2019-01-28 15:23:23 +0800

    調整

--- src/app/module/module.component.ts
+++ src/app/module/module.component.ts
@@ -9,17 +9,15 @@ import { Validators, FormBuilder } from '@angular/forms';
 export class ModuleComponent {
   form;
 
-  // Form 表單 Model  //在這裡指定我的form的驗證的規則 required minLength 
-  constructor(private fb: FormBuilder) 
-  {
+  // Form 表單 Model  //在這裡指定我的form的驗證的規則 required minLength
+  constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       account: ['', [Validators.required, Validators.minLength(5)]],
       password: ['', [Validators.required]],
-    })
-}
+    });
+  }
 
-  //前略  
   reset() {
-    this.form.reset(); //使用formGroup提供的reset方法:重置FormGroup
+    this.form.reset(); // 使用formGroup提供的reset方法:重置FormGroup
   }
 }
